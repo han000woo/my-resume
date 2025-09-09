e142bfb395ba4c1702a4a7209cd47ae3477280d8
한선우 <gkstjsdn0011@naver.com>
2025-09-10 01:24:38 +0900

    이력서 문구 수정, css수정 클릭불가

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,15 +14,44 @@ const Header = () => {
     <header className="main-header">
       <div className="header-content">
         <p className="tagline">
-          안녕하십니까<br />
-          <span id="typing-text"></span><span id="cursor">|</span>
+          안녕하십니까
+          <br />
+          <span id="typing-text"></span>
+          <span id="cursor">|</span>
           한선우 입니다
         </p>
+        <p className="self-instroduction">
+          군대 싸지방에서 처음으로 코딩에 관심을 가지게 되었고, 그 후 독학으로
+          프로그래밍을 공부하기 시작했습니다.
+          <br />
+          이후, 개발자가 되기 위해 컴퓨터 공학을 복수 전공하며 한국정보교육원과
+          KOSTA 부트캠프에서 웹 개발자로서의 역량을 키웠습니다.
+          <br />
+          앞으로도 지속적으로 배우고 성장하는 자세로 더 나은 개발자가
+          되겠습니다.
+        </p>
+
         <div className="contact-info">
-          <span><i className="fas fa-envelope"></i> gkstjsdn0011@naver.com</span>
-          <span><i className="fas fa-phone"></i> 010-2466-7537</span>
-          <span onClick={showMap}><i className="fas fa-map-marker-alt"></i> 서울 은평구 연서로43길 9-21</span>
-          <span><i className="fab fa-github"></i> <a href="https://github.com/han000woo" target="_blank" rel="noopener noreferrer">github.com/han000woo</a></span>
+          <span>
+            <i className="fas fa-envelope"></i> gkstjsdn0011@naver.com
+          </span>
+          <span>
+            <i className="fas fa-phone"></i> 010-2466-7537
+          </span>
+          <span onClick={showMap}>
+            <i className="fas fa-map-marker-alt"></i> 서울 은평구 연서로43길
+            9-21
+          </span>
+          <span>
+            <i className="fab fa-github"></i>{" "}
+            <a
+              href="https://github.com/han000woo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github.com/han000woo
+            </a>
+          </span>
         </div>
       </div>
     </header>
